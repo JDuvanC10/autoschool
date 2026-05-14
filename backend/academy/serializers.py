459b3a640cef59b17519c8a4a202a1705605c708
Duvan Carvajal <jduvancarvajal10@gmail.com>
2026-05-13 20:13:04 -0500

    Feat: Validacion del StudentPictureSerializer tamaño max de 2mb y tipo de archivo

--- backend/academy/serializers.py
+++ backend/academy/serializers.py
@@ -8,8 +8,23 @@ class StudentSerializer(serializers.ModelSerializer):
 
 class StudentPictureSerializer(serializers.ModelSerializer):
     def validate_profile_picture(self, value):
-        # TODO(actividad): Implementar validaciones de archivo (tamano y tipo MIME).
-        # Ejemplo: permitir image/jpeg e image/png y limitar a 2MB.
+        max_size = 2 * 1024 * 1024
+
+        if value.size > max_size:
+            raise serializers.ValidationError(
+                "La imagen excede 2MB."
+            )
+
+        allowed_types = [
+            'image/jpeg',
+            'image/png'
+        ]
+
+        if value.content_type not in allowed_types:
+            raise serializers.ValidationError(
+                "Solo se permiten imágenes JPEG y PNG."
+            )
+
         return value
 
     class Meta:
